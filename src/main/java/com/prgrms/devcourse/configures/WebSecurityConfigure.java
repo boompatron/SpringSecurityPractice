5b92915b37694eb079f10e07c937f9dcc2566c2a
boompatron <grapeflavoredapple@gmail.com>
2022-12-23 17:03:16 +0900

    error

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -2,22 +2,19 @@ package com.prgrms.devcourse.configures;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.AccessDecisionVoter;
-import org.springframework.security.access.expression.SecurityExpressionHandler;
-import org.springframework.security.access.vote.UnanimousBased;
-import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -26,6 +23,7 @@ import org.springframework.security.web.access.expression.WebExpressionVoter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,32 +34,65 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    @Bean
-    @Qualifier("myAsyncTaskExecutor")
-    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(3);
-        executor.setMaxPoolSize(5);
-        executor.setThreadNamePrefix("my-executor-");
-        return executor;
-    }
+    private final DataSource dataSource;
 
-    @Bean
-    public DelegatingSecurityContextAsyncTaskExecutor taskExecutor(
-            @Qualifier("myAsyncTaskExecutor") ThreadPoolTaskExecutor delegate
-    ) {
-        return new DelegatingSecurityContextAsyncTaskExecutor(delegate);
+    public WebSecurityConfigure(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}user123").roles("USER")
-                .and()
-                .withUser("admin01").password("{noop}admin123").roles("ADMIN")
-                .and()
-                .withUser("admin02").password("{noop}admin123").roles("ADMIN")
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/assets/**", "/h2-console/**");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "SELECT " +
+                                "login_id, passwd, true " +
+                                "FROM " +
+                                "users " +
+                                "WHERE " +
+                                "login_id = ?"
+                )
+                .groupAuthoritiesByUsername(
+                        "SELECT " +
+                                "u.login_id, g.name, p.name " +
+                                "FROM " +
+                                "users u JOIN groups g ON u.group_id = g.id " +
+                                "LEFT JOIN group_permission gp ON g.id = gp.group_id " +
+                                "JOIN permissions p ON p.id = gp.permission_id " +
+                                "WHERE " +
+                                "u.login_id = ?"
+                )
+                .getUserDetailsService().setEnableAuthorities(false)
         ;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        JdbcDaoImpl jdbcDao = new JdbcDaoImpl();
+        jdbcDao.setDataSource(dataSource);
+        jdbcDao.setEnableAuthorities(false);
+        jdbcDao.setEnableGroups(false);
+        jdbcDao.setUsersByUsernameQuery(
+                "SELECT login_id, passwd, true " +
+                "FROM users " +
+                "WHERE login_id = ?"
+        );
+        jdbcDao.setGroupAuthoritiesByUsernameQuery(
+                "SELECT u.login_id, g.name, p.name " +
+                "FROM " +
+                "users u JOIN groups g ON u.group_id = g.id " +
+                "LEFT JOIN group_permission gp ON g.id = gp.group_id " +
+                "JOIN permissions p ON p.id = gp.permission_id " +
+                "WHERE " +
+                "u.login_id = ?"
+        );
+
+        return jdbcDao;
     }
 
     @Bean
@@ -79,21 +110,8 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public AccessDecisionManager accessDecisionManager(){
-        List<AccessDecisionVoter<?>> voters = new ArrayList<>();
-        voters.add(new WebExpressionVoter());
-        voters.add(new OddAdminVoter(new AntPathRequestMatcher("/admin")));
-
-        return new UnanimousBased(voters);
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/assets/**");
-    }
-
-    public SecurityExpressionHandler<FilterInvocation> expressionHandler() {
-        return new CustomWebSecurityExpressionHandler(new AuthenticationTrustResolverImpl(), "ROLE_");
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
@@ -102,15 +120,16 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/me").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/admin").access("isFullyAuthenticated() and hasRole('ADMIN')")
-                //.antMatchers("/admin").access("isFullyAuthenticated() and hasRole('ADMIN')")
-
                 .anyRequest().permitAll()
-                .accessDecisionManager(accessDecisionManager())
-                // .expressionHandler(expressionHandler())
                 .and()
                 .formLogin()
                 .defaultSuccessUrl("/")
                 .permitAll()
+                .and()
+                /**
+                 * Basic Authentication 설정
+                 */
+                .httpBasic()
                 .and()
                 /**
                  * remember me 설정
@@ -141,4 +160,5 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
                 .accessDeniedHandler(accessDeniedHandler())
         ;
     }
+
 }
